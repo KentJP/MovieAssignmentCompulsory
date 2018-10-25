e7cbe0a56bc707e709cd65a56555ddaea3595705
fjt1509 <fjt1509@hotmail.com>
2018-10-25 12:21:20 +0200

    service imple interface

--- MovieRatingAssignment.Core/ApplicationService/Service/MovieReviewService.cs
+++ MovieRatingAssignment.Core/ApplicationService/Service/MovieReviewService.cs
@@ -1,4 +1,5 @@
 ﻿using MovieRatingAssignment.Core.ApplicationService.IService;
+using MovieRatingAssignment.Core.Entity;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,89 @@ namespace MovieRatingAssignment.Core.ApplicationService.Service
 {
     public class MovieReviewService : IMovieReviewService
     {
+        public int getAmountOfSpecificRatingForMovie(int id, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getAverageRateFromReviewer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getAverageRatingForMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> getIdsFromHighestRatedMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getMostActiveReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getMoviesRatedByReviewers(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getNumberOfReviewsFromReviewer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getNumberOfReviewsWithReviewValueFromReviewer(int id, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getReviewAmountForMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> getReviewersHavingRatedSpecificMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getTopMoviesFromInput(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getMostActiveReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getMostActiveReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getMoviesRatedByReviewers(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getMoviesRatedByReviewers(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getTopMoviesFromInput(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<MovieReview> IMovieReviewService.getTopMoviesFromInput(int value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
