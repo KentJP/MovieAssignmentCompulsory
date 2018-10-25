3c0b8087ad647d142bf03ffd9c2f0f348a28a7cc
fjt1509 <fjt1509@hotmail.com>
2018-10-25 12:02:49 +0200

    test setup

---
+++ XUnitTestMovieRating/PerformanceTestMovieReviewService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestMovieRating
+{
+    public class PerformanceTestMovieReview
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ XUnitTestMovieRating/UnitTestMovieReviewService.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestMovieRating
+{
+    public class UnitTestMovieReviewService
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
