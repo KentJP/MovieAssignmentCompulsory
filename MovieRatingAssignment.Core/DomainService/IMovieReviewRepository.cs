f0e05a07aee5b38df04a2703c5eac013aa1c998c
fjt1509 <fjt1509@hotmail.com>
2018-10-25 11:55:59 +0200

    setup

--- MovieRatingAssignment.Core/DomainService/IMovieReviewRepository.cs
+++ MovieRatingAssignment.Core/DomainService/IMovieReviewRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieRatingAssignment.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,37 @@ namespace MovieRatingAssignment.Core.DomainService
 {
     public interface IMovieReviewRepository
     {
+        //1.
+        int getNumberOfReviewsFromReviewer(int id);
+
+        //2.
+        double getAverageRateFromReviewer(int id);
+
+        //3.
+        int getNumberOfReviewsWithReviewValueFromReviewer(int id, int grade);
+
+        //4.
+        int getReviewAmountForMovie(int id);
+
+        //5.
+        double getAverageRatingForMovie(int id);
+
+        //6.
+        int getAmountOfSpecificRatingForMovie(int id, int grade);
+
+        //7.
+        List<int> getIdsFromHighestRatedMovies();
+
+        //8.
+        List<MovieReview> getMostActiveReviewers();
+
+        //9.
+        List<MovieReview> getTopMoviesFromInput(int value);
+
+        //10.
+        List<MovieReview> getMoviesRatedByReviewers(int id);
+
+        //11.
+        List<int> getReviewersHavingRatedSpecificMovie(int id); 
     }
 }
--- MovieRatingAssignment.Infrastructure/Repository/MovieReviewRepository.cs
+++ MovieRatingAssignment.Infrastructure/Repository/MovieReviewRepository.cs
@@ -67,5 +67,60 @@ namespace MovieRatingAssignment.Infrastructure.Repository
             }
             return m;
         }
+
+        public int getAmountOfSpecificRatingForMovie(int id, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getAverageRateFromReviewer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getAverageRatingForMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> getIdsFromHighestRatedMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getMostActiveReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getMoviesRatedByReviewers(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getNumberOfReviewsFromReviewer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getNumberOfReviewsWithReviewValueFromReviewer(int id, int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getReviewAmountForMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> getReviewersHavingRatedSpecificMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieReview> getTopMoviesFromInput(int value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
